bb3d5f53d4235b67ba46c98cc23f90be9cdf2086
JoaovPazn <joaovitor223344joao@gmail.com>
2026-05-22 09:23:21 -0300

    Add files via upload

---
+++ sistema-de-solicitacao/src/test/java/br/edu/ifpe/sistema_de_solicitacao/SistemaDeSolicitacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.sistema_de_solicitacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeSolicitacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
