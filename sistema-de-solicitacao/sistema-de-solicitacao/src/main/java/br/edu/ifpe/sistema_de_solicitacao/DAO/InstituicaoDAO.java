2f40a6573ce18fa47f8ccf17c98960704ca022c3
JoaovPazn <joaovitor223344joao@gmail.com>
2026-06-26 18:04:20 -0300

    Delete sistema-de-solicitacao/sistema-de-solicitacao directory

--- sistema-de-solicitacao/sistema-de-solicitacao/src/main/java/br/edu/ifpe/sistema_de_solicitacao/DAO/InstituicaoDAO.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifpe.sistema_de_solicitacao.DAO;
-
-import br.edu.ifpe.sistema_de_solicitacao.Model.instituicao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InstituicaoDAO extends JpaRepository<instituicao, String> { 
-}
